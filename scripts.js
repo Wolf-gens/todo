1654d8c7d0ab2d76f0be79e573dd9c2466cc6941
Wolf-gens <wolfpaisible@gmail.com>
2025-07-08 19:11:40 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,43 @@ const addTaskBtn = document.querySelector("#add-task");
 const taskList = document.querySelector(".task-list");
 
 function createTask(text) {
-  const taskItem = document.createElement("div");
+  const taskItem = document.createElement("div") let taskInput = document.querySelector("#task-input");
+    let addTask = document.querySelector("#add-task");
+    let taskList = document.querySelector(".task-list");
+
+    addTask.addEventListener('click', () => {
+        let taskItem = document.createElement("p");
+        taskItem.setAttribute("class", "task-item");
+
+        let removeTask = document.createElement("button");
+        removeTask.setAttribute("class", "remove-task");
+
+        let checkedTask = document.createElement("input");
+        checkedTask.setAttribute("type", "checkbox"); 
+
+        if (taskInput.value.trim() !== "") {
+            let inputValue = taskInput.value;
+
+          
+            let taskText = document.createElement("span");
+            taskText.textContent = inputValue;
+
+            removeTask.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+
+            taskItem.appendChild(checkedTask);
+            taskItem.appendChild(taskText);
+            taskItem.appendChild(removeTask);
+            taskList.prepend(taskItem);
+        }
+
+        if (taskInput.value !== "") {
+            taskInput.value = "";
+        }  
+
+        removeTask.addEventListener("click", () => {
+            taskList.removeChild(taskItem);
+        });
+    });;
   taskItem.classList.add("task-item");
 
   const checkbox = document.createElement("input");
